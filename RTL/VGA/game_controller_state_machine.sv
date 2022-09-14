928efc80a7656bb791c8129d6b342cc1af522eda
Golan Gershonowitz <golan4263@gmail.com>
2022-09-14 14:36:00 +0300

    updated game_controller_SM with changes from the test branch

--- RTL/VGA/game_controller_state_machine.sv
+++ RTL/VGA/game_controller_state_machine.sv
@@ -16,7 +16,7 @@ module game_controller_SM
 	
 	
 
-	input 	logic collisionPulse,
+	//input 	logic collisionPulse,
 	
 	output 	logic winPulse,
 	output 	logic losePulse,	
@@ -28,7 +28,7 @@ module game_controller_SM
 	);
 	
 	// state machine decleration 
-	enum logic [2:0] {s_idle, s_stage_1, s_stage_2, s_win, s_lose, s_scored} game_ps, game_ns ;
+	enum logic [3:0] {s_idle, s_stage_1, s_stage_2, /***s_stage_3, s_stage_4,***/ s_win, s_lose, s_scored} game_ps, game_ns/***, s_stage_ps, s_stage_ns***/ ;
 	
 	logic oneSecPulseOut;
 	logic rst_cntN;
@@ -46,6 +46,9 @@ module game_controller_SM
 	
 	logic change_stage_ns, change_stage_ps;
 	logic score_up_ns, score_up_ps;
+	
+	logic wonFlag_ns, wonFlag_ps;
+	logic lostFlag_ns, lostFlag_ps;
 
 	
 	assign winPulse = winPulse_ps;
@@ -55,8 +58,8 @@ module game_controller_SM
 	assign stage_num = stage_num_ps;
 	
 	
-	logic [3:0] scoreLowLoad;
-	logic [3:0] scoreHighLoad;
+	logic [3:0] scoreLowLoad_ns, scoreLowLoad_ps;
+	logic [3:0] scoreHighLoad_ns, scoreHighLoad_ps;
 
 	logic [3:0] seconds_to_load;
 	
@@ -67,6 +70,7 @@ module game_controller_SM
 //	logic timeLoadN;
 
 	logic finished_counting;
+	
 	
 	
 	
@@ -89,26 +93,27 @@ module game_controller_SM
 							.tc( finished_counting )		// output
 	);
 	
-	up_counter stage_counter (
+	/*** up_counter stage_counter (
 							.clk( clk ), 
 							.resetN( resetN ),
 							.loadN( stageLoadN_ps ),
-							.enable1( change_stage ),
-							.enable2( change_stage ),
-							.enable3( change_stage ),
+							.enable1( change_stage_ps ),
+							.enable2( change_stage_ps ),
+							.enable3( change_stage_ps ),
 							.datain( stage_num_ps ),
 							.count( stage_num ),	// output
 							.tc(  )		// output
-	);
+							
+	);***/
 	
 	two_digits_decimal_up_counter score_counter (
 											.clk( clk ), 
 											.resetN( resetN ),
 											.loadN( scoreLoadN_ps ),
-											.enable1( score_up ),
-											.enable2( score_up ),
-											.datainL( scoreLowLoad ),
-											.datainH( scoreHighLoad ),
+											.enable1( score_up_ps ),
+											.enable2( score_up_ps ),
+											.datainL( scoreLowLoad_ps ),
+											.datainH( scoreHighLoad_ps ),
 											.countL( scoreL ),	// output
 											.countH( scoreH ),	// output
 											.tc(  )			// output
@@ -122,14 +127,16 @@ module game_controller_SM
 			begin
 				game_ps <= s_idle;
 				rst_cntN_ps 	<= 1'b1;
-				stage_num_ps 	<= 4'd1;
+				stage_num_ps 	<= 4'd0;
 				timeLoadN_ps 	<= 1'b1;
 				scoreLoadN_ps 	<= 1'b1;
 				stageLoadN_ps 	<= 1'b1;
-				winPulse_ns		<= winPulse_ps;
-				losePulse_ns	<= losePulse_ps;
-				score_up_ns 	<= score_up_ps;
-				change_stage_ns <= change_stage_ps;
+				winPulse_ps		<= 1'b0;
+				losePulse_ps	<= 1'b0;
+				score_up_ps 	<= 1'b0;
+				change_stage_ps <= 1'b1;
+				wonFlag_ps		<= 1'b0;
+				lostFlag_ps		<= 1'b0;
 				//count_seconds_ps <= 1'b0;
 			end
 		
@@ -146,6 +153,8 @@ module game_controller_SM
 				losePulse_ps	<= losePulse_ns;
 				score_up_ps 	<= score_up_ns;
 				change_stage_ps <= change_stage_ns;
+				wonFlag_ps		<= wonFlag_ns;
+				lostFlag_ps		<= lostFlag_ns;
 			end
 	end // always sync
 	
@@ -169,13 +178,17 @@ module game_controller_SM
 //		stageLoadN = 1'b1;
 //		timeLoadN = 1'b1;
 		
-//		scoreHighLoad 	= 4'd0;
-//		scoreLowLoad 	= 4'd0;
-		seconds_to_load =  4'd0;
+		scoreHighLoad_ns 	= 4'd0;
+		scoreLowLoad_ns 	= 4'd0;
+		seconds_to_load 	=  4'd0;
 		
 		
 		score_up_ns 		= 1'b0;
 		change_stage_ns 	= 1'b0;
+		
+		wonFlag_ns			= wonFlag_ps;
+		lostFlag_ns 		= lostFlag_ps;
+		
 		
 			
 		case (game_ps)
@@ -183,34 +196,65 @@ module game_controller_SM
 		s_idle: begin
 				rst_cntN_ns = 1'b1;
 				
-				if ( ballhole_collide[(`NUM_BALLS):1] != {(`NUM_BALLS){1'b0}} ) // scored!
+				if ( change_stage_ps )
+					begin
+						stage_num_ns = stage_num_ps + 4'd1;
+						//change_stage_ns = 1'b0;
+					end
+				else if ( ballhole_collide[(`NUM_BALLS):1] != {(`NUM_BALLS){1'b0}} ) // scored!
 					begin
 						game_ns = s_scored;
 					end
-				else if ( balls_in_game[(`NUM_BALLS):1] == {(`NUM_BALLS){1'b0}} ) // if only white stayed in game
+				else if ( balls_in_game[(`NUM_BALLS):1] == {(`NUM_BALLS){1'b0}} && balls_in_game[0] == 1'b1 && !wonFlag_ps ) // if only white stayed in game
 					begin
 						game_ns = s_win;
 						rst_cntN_ns = 1'b0;
 					end
-				else if ( ballhole_collide[0] == 1'b1 ) // white is out, lost
+				else if ( ballhole_collide[0] == 1'b1 && !lostFlag_ps ) // white is out, lost
 					begin
 						game_ns = s_lose;
 						rst_cntN_ns = 1'b0;
+					end
+				
+				if ( scoreLoadN_ps )
+					begin
+						scoreLoadN_ns = 1'b0;
 					end
 				end // idle
 			
 			s_stage_1: begin
 				/// make stage 1 setup ///
+				
 				stage_num_ns = 4'd1;
+				//s_stage_ns = 4'd1;
+				game_ns = s_idle;
 			end // stage 1
 			
 			s_stage_2: begin
 			/// make stage 2 setup ///
 				
 				stage_num_ns = 4'd2;
+				//s_stage_ns = 4'd2;
+				game_ns = s_idle;
 			end // stage 2
-						
-						
+			
+			/***
+			s_stage_3: begin
+			/// make stage 3 setup ///
+				
+				stage_num_ns = 4'd3;
+				s_stage_ns = 4'd3;
+				game_ns = s_idle;
+			end // stage 3
+			
+			s_stage_4: begin
+			/// make stage 4 setup ///
+				
+				stage_num_ns = 4'd4;
+				s_stage_ns = 4'd4;
+				game_ns = s_idle;
+			end // stage 4
+						***/
 			s_win: begin
 			
 				if ( winPulse_ps != 1'b1 )		
@@ -219,12 +263,14 @@ module game_controller_SM
 						timeLoadN_ns = 1'b0; // load
 						seconds_to_load = 4'd2; // load two seconds
 						winPulse_ns = 1'b1; 
+						wonFlag_ns = 1'b1;
 					end
 				
-				
-				if ( finished_counting == 1'b1 ) 
+				else if ( finished_counting == 1'b1 ) 
 					begin
 						winPulse_ns = 1'b0; 
+						//stage_num_ns = stage_num_ps + 4'd1;
+						change_stage_ns = 1'b1;
 						game_ns = s_idle;
 					end
 				end // win
@@ -232,16 +278,16 @@ module game_controller_SM
 			
 			s_lose: begin
 				
-			if ( losePulse_ps != 1'b1 )		
+				if ( losePulse_ps != 1'b1 )		
 					begin	
 						rst_cntN_ns = 1'b1; // turn reset off
 						timeLoadN_ns = 1'b0; // load
 						seconds_to_load = 4'd3; // load three seconds
 						losePulse_ns = 1'b1;
+						lostFlag_ns = 1'b1;
 					end
 				
-				
-				if ( finished_counting == 1'b1 ) 
+				else if ( finished_counting == 1'b1 ) 
 					begin
 						losePulse_ns = 1'b0;
 						game_ns = s_idle;
@@ -250,8 +296,9 @@ module game_controller_SM
 			
 			
 			s_scored: begin
-//				rst_cntN_ns = 1'b1;
-				score_up_ns = 1'b1;
+				scoreHighLoad_ns = scoreH + 4'd1;
+				scoreLowLoad_ns = scoreL + 4'd0;
+				scoreLoadN_ns = 1'b0;
 				game_ns = s_idle;
 				
 //				if ( winPulse == 1'b1 ) // if won
